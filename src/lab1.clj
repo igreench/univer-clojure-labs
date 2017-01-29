1ffa804423e06d9fdcc5c955a131a53521a86308
Evgeny Lipkin <elipkin@cisco.com>
2017-01-29 09:40:07 +0700

    [Test] updated tests

--- src/lab1.clj
+++ src/lab1.clj
@@ -1,6 +1,11 @@
-(ns lab1)
+(ns lab1
+  (:use clojure.test))
 
-(defn words [data, n]
+(defn conj*
+  [s x]
+  (conj (vec s) x))
+
+(defn permutation [data, n]
   (reduce
     (fn [result, i]
       (apply concat
@@ -10,7 +15,7 @@
               (fn [data-element]
                 (if (= i 0)
                   (list result-element data-element)
-                  (concat result-element data-element)))
+                  (conj* result-element data-element)))
               (filter
                 (fn [element]
                   (if (= i 0)
@@ -26,22 +31,16 @@
     data
     (range (- n 1))))
 
-(println (words (list "a" "b" "c") 3))
-;(println (words (list "a" "b" 4) 3))
-;(println (words (list "a" 11 false) 3))
-; (println (words (list "a" 11 [33 44]) 3))
-; (println (words (list "a" 11 [33 44] false) 4))
+(deftest tests
+  (testing "lab1 tests"
+    (is (= (permutation (list "a" "b" "c") 3)
+           (list ["a" "b" "a"] ["a" "b" "c"] ["a" "c" "a"] ["a" "c" "b"] ["b" "a" "b"] ["b" "a" "c"] ["b" "c" "a"] ["b" "c" "b"] ["c" "a" "b"] ["c" "a" "c"] ["c" "b" "a"] ["c" "b" "c"])))
+    (is (= (permutation (list "a" "b" 4) 3)
+           (list ["a" "b" "a"] ["a" "b" 4] ["a" 4 "a"] ["a" 4 "b"] ["b" "a" "b"] ["b" "a" 4] ["b" 4 "a"] ["b" 4 "b"] [4 "a" "b"] [4 "a" 4] [4 "b" "a"] [4 "b" 4])))
+    (is (= (permutation (list "a" 11 false) 3)
+           (list ["a" 11 "a"] ["a" 11 false] ["a" false "a"] ["a" false 11] [11 "a" 11] [11 "a" false] [11 false "a"] [11 false 11] [false "a" 11] [false "a" false] [false 11 "a"] [false 11 false])))
+    (is (= (permutation (list "a" 11 [33 44]) 3)
+           (list ["a" 11 "a"] ["a" 11 [33 44]] ["a" [33 44] "a"] ["a" [33 44] 11] [11 "a" 11] [11 "a" [33 44]] [11 [33 44] "a"] [11 [33 44] 11] [[33 44] "a" 11] [[33 44] "a" [33 44]] [[33 44] 11 "a"] [[33 44] 11 [33 44]])))
+    ))
 
-;(println (list "a"))
-;(println (list (list "a")))
-;(println (cons "11" (list "a")))
-
-;(println (list (list "a")))
-;(println (list (list "c") (list "d")))
-;
-;(println (cons "e" (list (list "a"))))
-;
-;(println (apply cons (list (list "a")) (list (list "c") (list "d"))))
-
-; empty list
-; mapcat вместо flatten и str
+(tests)
--- src/lab4.test.clj
+++ src/lab4.test.clj
@@ -1,38 +1,36 @@
-(ns
-
-  lab4tests
+(ns lab4tests
   (:use lab4)
-  (:require [clojure.test :as test]))
+  (:use clojure.test))
 
-(test/deftest lab4tests
-  (test/testing "lab4 tests"
-    (test/is (= 4 (+ 2 2)))
+(deftest lab4tests
+  (testing "lab4 tests"
+    (is (= 4 (+ 2 2)))
 
-    (test/is (= (constant true) (solve-subst (run (_imp (_and (variable :a) (_not (_or (_imp (variable :a) (variable :b)) (variable :c)))) (_and (variable :c) (variable :c) (variable :a)))))))
+    (is (= (constant true) (solve-subst (run (_imp (_and (variable :a) (_not (_or (_imp (variable :a) (variable :b)) (variable :c)))) (_and (variable :c) (variable :c) (variable :a)))))))
 
-    (test/is (= (_and
+    (is (= (_and
                   (_not (variable :x))
                   (_not (variable :y)))
                (de-morg (_not (_or (variable :x) (variable :y))))))
 
-    (test/is (= (variable :z)
+    (is (= (variable :z)
                 (disc-not (_not (_not (variable :z))))))
 
-    (test/is (= (run (variable :x)) (variable :x)))
-    (test/is (= (run (_imp (variable :x) (variable :y))) (_or (_not (variable :x)) (variable :y))))
-    (test/is (= (run (_and (_or (variable :a) (variable :b)) (_or (variable :x) (variable :y))))
+    (is (= (run (variable :x)) (variable :x)))
+    (is (= (run (_imp (variable :x) (variable :y))) (_or (_not (variable :x)) (variable :y))))
+    (is (= (run (_and (_or (variable :a) (variable :b)) (_or (variable :x) (variable :y))))
            (_or (_and (variable :a) (variable :x))
                 (_and (variable :a) (variable :y))
                 (_and (variable :b) (variable :x))
                 (_and (variable :b) (variable :y)))))
-    (test/is (= (run (constant true)) (constant true)))
-    (test/is (= (run (constant false)) (constant false)))
-    (test/is (= (run (_imp (_imp (variable :x) (_or (variable :z) (_not (variable :x)) (variable :z))) (_imp (variable :z) (variable :y))))
+    (is (= (run (constant true)) (constant true)))
+    (is (= (run (constant false)) (constant false)))
+    (is (= (run (_imp (_imp (variable :x) (_or (variable :z) (_not (variable :x)) (variable :z))) (_imp (variable :z) (variable :y))))
                 (_or (_and (variable :x) (_not (variable :z)) (variable :x) (_not (variable :z))) (_not (variable :z)) (variable :y)))
              )
     )
   )
-(test/run-tests)
+(run-tests)
 
 
 
