4db4c03c5a03e4c02e59e50bf3184b5a0a8917be
Evgeny Lipkin <elipkin@cisco.com>
2017-01-29 08:56:50 +0700

    [Fix] lab1 by lists

--- src/lab1.clj
+++ src/lab1.clj
@@ -3,41 +3,45 @@
 (defn words [data, n]
   (reduce
     (fn [result, i]
-      (flatten
+      (apply concat
         (map
           (fn [result-element]
             (map
               (fn [data-element]
-                (println "WHO:")
-                (println result-element)
-                (println data-element)
                 (if (= i 0)
-                  (list data-element)
-                  (cons result-element data-element))
-                ;(str result-element data-element)
-                )
+                  (list result-element data-element)
+                  (concat result-element data-element)))
               (filter
                 (fn [element]
-                  (println "WHAT:")
-                  (println result-element)
-                  (println element)
-                  ;(not=
-                  ;  (last result-element)
-                  ;  element)
-
                   (if (= i 0)
-                    true
+                    (not=
+                      result-element
+                      element)
                     (not=
                       (last result-element)
                       element))
                   )
                 data)))
-          result))
-      )
-    (list nil)
-    (range n)))
+          result)))
+    data
+    (range (- n 1))))
 
-(println (words (list "a" 1 [3 4]) 3))
+(println (words (list "a" "b" "c") 3))
+;(println (words (list "a" "b" 4) 3))
+;(println (words (list "a" 11 false) 3))
+; (println (words (list "a" 11 [33 44]) 3))
+; (println (words (list "a" 11 [33 44] false) 4))
+
+;(println (list "a"))
+;(println (list (list "a")))
+;(println (cons "11" (list "a")))
+
+;(println (list (list "a")))
+;(println (list (list "c") (list "d")))
+;
+;(println (cons "e" (list (list "a"))))
+;
+;(println (apply cons (list (list "a")) (list (list "c") (list "d"))))
 
 ; empty list
 ; mapcat вместо flatten и str
